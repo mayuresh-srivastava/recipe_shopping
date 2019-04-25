1ef43bc4a66fbaa92a207ec9b24cdbba3a72fd27
Mayuresh Srivastava <Mayuresh.Srivastava@rsystems.com>
2019-04-25 17:36:48 +0530

    Components and recipe model created.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    newRecipe('A Test Recipe', 'This is simply a test', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/bhtv_ep5_shakshuka11_0.jpg?itok=RZ2CPwM_&mtime=1516076383')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
